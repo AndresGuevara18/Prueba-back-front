89aa8748fa073e594e96fe1db901689c9562ef4d
andres-1891 <mauricio_acastro@soy.sena.edu.co>
2025-06-02 18:50:28 -0500

    se agrega ignore y confi pass

--- backend-node/src/config/database.js
+++ backend-node/src/config/database.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',  // Cambia según tu configuración
     user: 'root',       // Tu usuario de MySQL
     password: '',       // Tu contraseña de MySQL
-    //password: 'admin123',       // pass mechas
+    //password: 'admin123',       //  pass mechas
     database: 'colpryst_col3'  // Nombre de tu base de datos
 });
 
